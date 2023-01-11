fec1200b69f33d9262d74452fa41e18442ec64ee
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2023-01-11 16:58:37 -0500

    looping code along

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages = []
+    for (let i=0; i < names.length; i++) {
+        const str = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+        messages.push(str)
+    }
+    return messages;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num--;
+    }
+}
